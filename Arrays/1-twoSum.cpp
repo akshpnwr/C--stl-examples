160d2bab52fc1f64324a91ddb906c5fba9812c4e
Akash Panwar <akashpanwar9976@gmail.com>
2023-02-09 19:07:13 +0530

    Abstracted two sum into a function

--- Arrays/1-twoSum.cpp
+++ Arrays/1-twoSum.cpp
@@ -1,31 +1,11 @@
 #include <bits/stdc++.h>
 #include <map>
 using namespace std;
-int main()
+
+vector<int> towSum(vector<int> nums, int target)
 {
-
-    vector<int> nums{2, 7, 11, 15, 3};
-
-    int target = 10, val;
-
-    // int a, b;
-
-    // for (int i = 0; i < nums.size(); i++)
-    // {
-    //     for (int j = i + 1; j < nums.size(); j++)
-    //     {
-    //         if ((nums[i] + nums[j]) == target)
-    //         {
-    //             a = i;
-    //             b = j;
-    //             break;
-    //         }
-    //     }
-    // }
-
-    // cout << a << b;
-
     map<int, int> h;
+    int val;
 
     for (int i = 0; i < nums.size(); i++)
     {
@@ -36,7 +16,24 @@ int main()
             return {h[val], i};
         }
         else
-            h[val] = i;
+            h[nums[i]] = i;
+    }
+
+    return {};
+}
+
+int main()
+{
+
+    vector<int> nums{2, 7, 11, 15, 3};
+
+    int target = 14;
+
+    vector<int> res = towSum(nums, target);
+
+    for (auto &i : res)
+    {
+        cout << i;
     }
 
     return 0;
