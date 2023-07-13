34dcf511528a7c03a3b5bd44604272d0b892c8f8
akshpnwr <akashpanwar9976@gmail.com>
2023-07-13 21:00:34 +0530

    two pointer method for best stock

--- Arrays/121-bestTimeToSellStock.cpp
+++ Arrays/121-bestTimeToSellStock.cpp
@@ -1,50 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int twoPointerMethod(vector<int> &prices)
+{
+    int l = 0, r = 1;
+    int maxP = 0, tempP;
+
+    while (r < prices.size())
+    {
+        if (prices[l] < prices[r])
+        {
+            tempP = prices[r] - prices[l];
+            maxP = max(tempP, maxP);
+        }
+        else
+            l++;
+
+        r++;
+    }
+
+    return maxP;
+}
+
 int main()
 {
 
-    vector<int> prices = {7, 5, 3, 6, 4, 1};
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
 
-    int min = prices[0];
-    int minIndex = 0, index = 0;
-    for (auto &price : prices)
-    {
-        if (price < min)
-        {
-            min = price;
-            minIndex = index;
-        }
-        index++;
-    }
+    // two pointer method
+    cout << "Max profit = " << twoPointerMethod(prices);
 
-    cout << "Min Value : " << min << endl
-         << "Index : " << minIndex;
+    // Normal method
 
-    if (minIndex == prices.size() - 1)
-    {
-        cout << "\nNo transactions";
-        return 0;
-    }
+    // int min = prices[0];
+    // int minIndex = 0, index = 0;
+    // for (auto &price : prices)
+    // {
+    //     if (price < min)
+    //     {
+    //         min = price;
+    //         minIndex = index;
+    //     }
+    //     index++;
+    // }
 
-    int max = prices[index + 1];
-    int maxIndex = index + 1;
+    // cout << "Min Value : " << min << endl
+    //      << "Index : " << minIndex;
 
-    for (int i = minIndex; i < prices.size(); i++)
-    {
-        if (max < prices[i])
-        {
-            max = prices[i];
-            maxIndex = i;
-        }
-    }
+    // if (minIndex == prices.size() - 1)
+    // {
+    //     cout << "\nNo transactions";
+    //     return 0;
+    // }
 
-    cout << endl
-         << "Max Value : " << max << endl
-         << "Index : " << maxIndex;
+    // int max = prices[index + 1];
+    // int maxIndex = index + 1;
 
-    cout << "\nBuy at day : " << minIndex + 1;
-    cout << "\nSell at day : " << maxIndex + 1;
+    // for (int i = minIndex; i < prices.size(); i++)
+    // {
+    //     if (max < prices[i])
+    //     {
+    //         max = prices[i];
+    //         maxIndex = i;
+    //     }
+    // }
+
+    // cout << endl
+    //      << "Max Value : " << max << endl
+    //      << "Index : " << maxIndex;
+
+    // cout << "\nBuy at day : " << minIndex + 1;
+    // cout << "\nSell at day : " << maxIndex + 1;
 
     return 0;
 }
