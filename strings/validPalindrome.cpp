884756623422f605edb643fe28dedc97442c98eb
akshpnwr <akashpanwar9976@gmail.com>
2023-07-18 12:22:15 +0530

    Palindrome using two pointer and continue

--- strings/validPalindrome.cpp
+++ strings/validPalindrome.cpp
@@ -1,16 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isPalindrome(string str)
+{
+    int start = 0;
+    int end = str.size() - 1;
+
+    bool pal = true;
+
+    while (start <= end)
+    {
+        if (!isalnum(str[start]))
+        {
+            start++;
+            continue;
+        }
+        if (!isalnum(str[end]))
+        {
+            end--;
+            continue;
+        }
+        if (tolower(str[start]) != tolower(str[end]))
+        {
+            return false;
+        }
+        else
+        {
+            start++;
+            end--;
+        }
+    }
+    return true;
+}
+
 int main()
 {
 
     string str = "A man, a plan, a canal: Panama";
 
-    cout << str.length() << endl;
+    bool res = isPalindrome(str);
 
-    str.erase(remove(str.begin(), str.end(), ' '), str.end());
-
-    cout << str.length();
+    cout << res;
 
     return 0;
 }
