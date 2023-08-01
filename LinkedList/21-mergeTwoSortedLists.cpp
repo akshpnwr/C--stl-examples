29dde814b701afdb7983b6e65a88aa91b8dae22a
akshpnwr <akashpanwar9976@gmail.com>
2023-08-01 11:52:54 +0530

    unnecessary code in merge two sorted lists

--- LinkedList/21-mergeTwoSortedLists.cpp
+++ LinkedList/21-mergeTwoSortedLists.cpp
@@ -83,13 +83,11 @@ ListNode *sortList(ListNode *l1, ListNode *l2)
         if (l1->val <= l2->val)
         {
             tail->next = l1;
-            tail = l1;
             l1 = l1->next;
         }
         else
         {
             tail->next = l2;
-            tail = l2;
             l2 = l2->next;
         }
         tail = tail->next;
