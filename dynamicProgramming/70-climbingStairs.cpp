9cbc37efd4ecf498b4cee8afea5253beacbf3e5f
akshpnwr <akashpanwar9976@gmail.com>
2023-08-04 17:01:42 +0530

    Climbing stairs

---
+++ dynamicProgramming/70-climbingStairs.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int climbStairs(int n)
+{
+    int one = 1;
+    int two = 1;
+    int temp;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        temp = one;
+        one = one + two;
+        two = temp;
+    }
+    return one;
+}
+
+int main()
+{
+    cout << climbStairs(5);
+}
