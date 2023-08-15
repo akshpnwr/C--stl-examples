4b901e56e5f1b5220a923d3f37f854a473eaf5ea
akshpnwr <akashpanwar9976@gmail.com>
2023-08-15 20:37:22 +0530

    three sum: attempt #2 -> no duplicates

--- Arrays/15-3Sum.cpp
+++ Arrays/15-3Sum.cpp
@@ -1,6 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(vector<int> &nums, int low, int high)
+{
+    int pivot = nums[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (nums[j] < pivot)
+        {
+            i++;
+            swap(nums[i], nums[j]);
+        }
+    }
+
+    i++;
+    swap(nums[i], nums[high]);
+
+    return i;
+}
+
+void quickSort(vector<int> &nums, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(nums, low, high);
+
+        quickSort(nums, low, pi - 1);
+        quickSort(nums, pi + 1, high);
+    }
+}
 vector<vector<int>> res;
 
 void getTwoSum(vector<int> &nums, int target, int cur)
@@ -21,11 +58,34 @@ void getTwoSum(vector<int> &nums, int target, int cur)
 
 vector<vector<int>> threeSum(vector<int> &nums)
 {
+    int l, r, sum;
+
     vector<vector<int>> res;
 
     for (int i = 0; i < nums.size(); i++)
     {
-        getTwoSum(nums, -nums[i], i + 1);
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        l = i + 1;
+        r = nums.size() - 1;
+        while (l < r)
+        {
+            sum = nums[i] + nums[l] + nums[r];
+
+            if (sum > 0)
+                r--;
+            if (sum < 0)
+                l++;
+            if (sum == 0)
+            {
+                res.push_back({nums[i], nums[l], nums[r]});
+                l++;
+
+                while (nums[l] == nums[l - 1] && l < r)
+                    l++;
+            }
+        }
     }
 
     return res;
@@ -36,7 +96,16 @@ int main()
 
     vector<int> nums{-1, 0, 1, 2, -1, -4};
 
-    threeSum(nums);
+    quickSort(nums, 0, nums.size() - 1);
+
+    // for (auto &num : nums)
+    // {
+    //     cout << num << " ";
+    // }
+
+    vector<vector<int>> res;
+
+    res = threeSum(nums);
 
     for (auto &item : res)
     {
